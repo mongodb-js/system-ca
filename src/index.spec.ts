4d7167b6ecd9b6b472f6bc4b051f797b7ec98176
Anna Henningsen <anna@addaleax.net>
2022-02-15 16:20:35 +0100

    Fix tests on macos

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,6 +1,6 @@
 import { expect } from 'chai';
 import { X509Certificate } from 'crypto';
-import { systemCertsSync, systemCertsAsync, Options } from './';
+import { systemCertsSync, systemCertsAsync, Options } from '../';
 
 function validateHasCertificate(certs: string[], serialNumber: string): void {
   if (!X509Certificate) { // Only available on Node.js 15.6.0+
@@ -11,7 +11,6 @@ function validateHasCertificate(certs: string[], serialNumber: string): void {
 }
 
 const verisignRootCA = '401AC46421B31321030EBBE4121AC51D';
-const hongKongPostCA = '08165F8A4CA5EC00C99340DFC4C6AE23B81C5AA4';
 
 describe('system-ca', () => {
   context('sync variant', () => {
